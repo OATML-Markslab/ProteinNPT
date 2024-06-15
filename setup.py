2934ce5216d0dfef4af40a7c9a6ce3962b3bd71b
Pascal Notin <pascal.notin@stx.ox.ac.uk>
2024-06-15 22:06:17 +0100

    Added MANIFEST.in

--- setup.py
+++ setup.py
@@ -8,8 +8,12 @@ setup(
     description="ProteinNPT: Improving Protein Property Prediction and Design with Non-Parametric Transformers",
     long_description=readme,
     long_description_content_type="text/markdown",
+    include_package_data=True,
+    package_data={
+        'proteinnpt': ['proteinnpt/utils/tranception/utils/tokenizers/Basic_tokenizer']
+    },
     author="Pascal Notin and Ruben Weitzman",
-    version="1.5",
+    version="1.6",
     license="MIT",
     url="https://github.com/OATML-Markslab/ProteinNPT",    
     packages=find_packages()
